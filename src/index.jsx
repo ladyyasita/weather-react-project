fe4f16c7b0d6f6ed83e48ea53996107d25229390
ladyyasita <121614535+ladyyasita@users.noreply.github.com>
2025-02-06 00:25:22 +0100

    project added from the sandbox

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,24 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
-    <footer>This project was coded by <a href="/" tafget="
-    _blank" rel="noopener noreferrer">Iaroslava Pozniak</a></footer>
+    <footer>
+      This project was coded by{" "}
+      <a
+        href="/"
+        tafget="
+    _blank"
+        rel="noopener noreferrer"
+      >
+        Iaroslava Pozniak
+      </a>
+      and is
+      <a
+        href="https://github.com/ladyyasita/weather-react-project"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        open-sourced on GitHub
+      </a>
+    </footer>
   </StrictMode>
 );
