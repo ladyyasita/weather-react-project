b08469483d2bbeb8ac9cd06f4ad3b6e99758d257
ladyyasita <121614535+ladyyasita@users.noreply.github.com>
2025-02-08 20:27:54 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -11,26 +11,20 @@ root.render(
     <App />
     <footer>
       This project was coded by{" "}
-      <a
-        href="/"
-        tafget="
-    _blank"
-        rel="noopener noreferrer"
-      >
+      <a href="/" target="_blank" rel="noopener noreferrer">
         Iaroslava Pozniak
-      </a>
-      and is
+      </a>{" "}
+      and is{" "}
       <a
         href="https://github.com/ladyyasita/weather-react-project"
         target="_blank"
         rel="noopener noreferrer"
       >
-        {" "}
         open-sourced on GitHub
-      </a>
-       and 
-       <a href="" target="_blank" rel="noopener noreferrer">
-         hosted on Netlify
+      </a>{" "}
+      and{" "}
+      <a href="" target="_blank" rel="noopener noreferrer">
+        hosted on Netlify
       </a>
     </footer>
   </StrictMode>
