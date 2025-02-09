5afc0b7d43a7dbe3a5ff090f142173fb3b341d37
ladyyasita <121614535+ladyyasita@users.noreply.github.com>
2025-02-09 20:01:46 +0100

    changed

--- src/index.jsx
+++ src/index.jsx
@@ -12,24 +12,44 @@ root.render(
     <footer>
       This project was coded by{" "}
       <a href="/" target="_blank" rel="noopener noreferrer">
-        {" "}
-        Iaroslava Pozniak{" "}
+        Iaroslava Pozniak
       </a>{" "}
-      and is{" "}
+      and is
       <a
         href="https://github.com/ladyyasita/weather-react-project"
         target="_blank"
         rel="noopener noreferrer"
       >
         {" "}
-        open-sourced on GitHub{" "}
+        open-sourced on GitHub
       </a>{" "}
-      and{" "}
+      and
       <a href="" target="_blank" rel="noopener noreferrer">
         {" "}
-        hosted on Netlify{" "}
+        hosted on Netlify
       </a>
       .
     </footer>
   </StrictMode>
 );
+<footer>
+  This project was coded by{" "}
+  <a href="/" target="_blank" rel="noopener noreferrer">
+    Iaroslava Pozniak
+  </a>{" "}
+  and is
+  <a
+    href="https://github.com/ladyyasita/weather-react-project"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    {" "}
+    open-sourced on GitHub
+  </a>{" "}
+  and
+  <a href="" target="_blank" rel="noopener noreferrer">
+    {" "}
+    hosted on Netlify
+  </a>
+  .
+</footer>;
