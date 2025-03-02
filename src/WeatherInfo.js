0748a8bef03197af53debd1d7d3bc811e414ec47
ladyyasita <121614535+ladyyasita@users.noreply.github.com>
2025-03-02 17:17:50 +0100

    fixed a lot

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -3,7 +3,7 @@ import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
 
-export default function WeathInfo(props) {
+export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
@@ -19,10 +19,9 @@ export default function WeathInfo(props) {
             <div>
               <WeatherIcon
                 code={props.data.icon}
-                alt={props.daata.description}
+                alt={props.data.description}
               />
             </div>
-
             <div>
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
@@ -30,8 +29,8 @@ export default function WeathInfo(props) {
         </div>
         <div className="col-6">
           <ul>
-            <li>Humidity:{weatherData.humidity}%</li>
-            <li>Wind: {weatherData.wind} km/h</li>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
           </ul>
         </div>
       </div>
